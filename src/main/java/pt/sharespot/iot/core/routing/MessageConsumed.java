f4c4ee6fd39a33f4d6e095b1e7f84670dfe0b81c
FilipeMCruz <1170911@isep.ipp.pt>
2022-02-23 00:41:08 +0000

    Added id to messages and ttl param

--- src/main/java/pt/sharespot/iot/core/routing/MessageConsumed.java
+++ src/main/java/pt/sharespot/iot/core/routing/MessageConsumed.java
@@ -2,16 +2,40 @@ package pt.sharespot.iot.core.routing;
 
 import pt.sharespot.iot.core.routing.keys.RoutingKeys;
 
+import java.util.Optional;
+import java.util.UUID;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+
 public class MessageConsumed<T> {
 
+    public UUID oid;
+
     public RoutingKeys routingKeys;
+
     public T data;
 
-    public MessageConsumed(RoutingKeys routingKeys, T data) {
-        this.routingKeys = routingKeys;
-        this.data = data;
-    }
+    public int hops;
 
     public MessageConsumed() {
+    }
+
+    public <A> MessageSupplied<A> toSupplied(Function<T, A> updateData) {
+        return new MessageSupplied<>(this, updateData.apply(data), routingKeys);
+    }
+
+    /**
+     * High-Order Function that receives two function that will alter the data and routing keys of the message
+     * First data is updated and then the routing keys are updated (so that we can use something like builder.withUpdated(data) from the outside)
+     *
+     * @param updateData        function that receives all needed info to run updates to data
+     * @param updateRoutingKeys function that receives all needed info to run updated to routing keys
+     * @param <A>               data type in message supplied
+     * @return if everything goes well, new message supplied updated, else empty object
+     */
+    public <A> Optional<MessageSupplied<A>> toSupplied(BiFunction<T, RoutingKeys, Optional<A>> updateData, BiFunction<A, RoutingKeys, Optional<RoutingKeys>> updateRoutingKeys) {
+        return updateData.apply(data, routingKeys)
+                .flatMap(data -> updateRoutingKeys.apply(data, this.routingKeys)
+                        .map(keys -> new MessageSupplied<>(this, data, keys)));
     }
 }
--- src/main/java/pt/sharespot/iot/core/routing/MessageSupplied.java
+++ src/main/java/pt/sharespot/iot/core/routing/MessageSupplied.java
@@ -2,16 +2,34 @@ package pt.sharespot.iot.core.routing;
 
 import pt.sharespot.iot.core.routing.keys.RoutingKeys;
 
-public class MessageSupplied<T> {
+import java.util.UUID;
+
+public class MessageSupplied<A> {
+
+    public static int TIME_TO_LIVE = 10;
+
+    public UUID oid;
 
     public RoutingKeys routingKeys;
-    public T data;
 
-    public MessageSupplied(RoutingKeys routingKeys, T data) {
+    public A data;
+
+    public int hops;
+
+    public MessageSupplied(RoutingKeys routingKeys, A data) {
+        this.oid = UUID.randomUUID();
+        this.hops = 0;
         this.routingKeys = routingKeys;
         this.data = data;
     }
 
-    public MessageSupplied() {
+    public <T> MessageSupplied(MessageConsumed<T> message, A updateData, RoutingKeys updateRoutingKeys) {
+        this.oid = message.oid;
+        this.hops = message.hops + 1;
+        this.routingKeys = updateRoutingKeys;
+        this.data = updateData;
+        if (this.hops >= TIME_TO_LIVE) {
+            throw new RuntimeException("Message has exceeded max time to live");
+        }
     }
 }
