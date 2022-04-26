87b8e00f2dcd6da94a14b77d0900bab664c0d82e
FilipeMCruz <1170911@isep.ipp.pt>
2022-04-26 11:47:07 +0100

    Started new version cycle

---
+++ src/main/java/pt/sharespot/iot/core/routing/exchanges/IoTCoreExchanges.java
@@ -0,0 +1,8 @@
+package pt.sharespot.iot.core.routing.exchanges;
+
+public class IoTCoreExchanges {
+
+    public static final String DATA_EXCHANGE = "sensor.topic";
+    
+    public static final String INTERNAL_EXCHANGE = "internal.topic";
+}
